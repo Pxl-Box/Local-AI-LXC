c45074db1ae1b7badca06e7fadc2cd7ad2358604
Conor Hudson <conorbrynhudson@gmail.com>
2026-03-16 16:19:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/api/images', (req, res) => {
 });
 
 // Serve the frontend for any other route (SPA)
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
